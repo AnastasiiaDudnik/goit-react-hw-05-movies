5f6af3353e67d1ebd8e26234172de3c0f25e4f76
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-03-27 00:38:28 +0300

    lazy import fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,8 +5,8 @@ import { Layout } from './Layout/Layout';
 const Home = lazy(() => import('../pages/Home'));
 const MovieSearch = lazy(() => import('../pages/MovieSearch'));
 const MovieDetails = lazy(() => import('../pages/MovieDetails'));
-const Cast = lazy(() => import('../components/Cast'));
-const Review = lazy(() => import('../components/Home'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Review = lazy(() => import('./Review/Review'));
 
 export const App = () => {
   return (
